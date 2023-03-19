80be2c2f0607e20cb55e125d65c6dccd4dbbe5f6
Allan Losenge <temlosenge@gmail.com>
2023-03-19 05:57:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(games) {
+    const winningGame = games.find(game => game.result === "W");
+    return winningGame ? winningGame.year : undefined;
+  }
+  
